da1ef724360641442e8eae88360e1e658089f36c
k-uzin <113524983+k-uzin@users.noreply.github.com>
2024-04-05 12:45:02 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,8 +1,5 @@
 $(function () {
-    Fancybox.bind("[data-fancybox]", { 
-        // Your options go here 
-    });
-    
+
     //카운트 업 함수
     $(function () {
         $('.num').each(function () {
@@ -21,24 +18,72 @@ $(function () {
     // 사이트 접속 시, 스크롤을 0.2초 동안 맨 위로 올림.
     gsap.to(window, 0.2, { scrollTo: 0 });
 
+    //모바일 메뉴 클릭시 메뉴 나오기
+    $('.mobile_nav button').click(function(){
+        $('.mobile_nav ul').toggleClass('on');
+    });
+
+    //스크롤 시 헤더 스타일 변경
+    let header = document.querySelector(".header");
+    window.addEventListener('scroll', () => {
+        let scrollBar = window.scrollY;
+        if (scrollBar > 0) {
+            header.classList.add('on'); /* .classList.add('on') > .on 아니고 점 안 붙인 채로 기입 */
+        } else {
+            header.classList.remove('on');
+        }
+    });
+
     //스크롤시 배경색 변경 (스크롤 값 백분율 계산 -> 반응형 대응)
     $(document).ready(function () {
-    
         $(document).scroll(function () {
-            var document_height = $(document).height(); //문서 전체의 높이
-            var scroll_pos = $(this).scrollTop();
-            var scroll_percent = (scroll_pos / (document_height - $(window).height())) * 100;
-    
+            let document_height = $(document).height(); //문서 전체의 높이
+            let scroll_pos = $(this).scrollTop();
+            let scroll_percent = (scroll_pos / (document_height - $(window).height())) * 100;
+
             console.log(scroll_percent);
-    
-            if (scroll_percent >= 30 && scroll_percent < 65 || scroll_percent >= 90) {
-                $("body").css('background-color', '#000000');
-            } else if (scroll_percent >= 65 && scroll_percent < 90) {
-                $("body").css('background-color', '#ffffff');
+
+            if ($(window).width() <= 767) {
+                if (scroll_percent >= 23 && scroll_percent < 83 || scroll_percent >= 99) {
+                    $("body").css('background-color', '#000000');
+                } else if (scroll_percent >= 83 && scroll_percent < 99) {
+                    $("body").css('background-color', '#ffffff');
+                } else {
+                    $("body").css('background-color', '#ffffff');
+                }
+            } else if ($(window).width() <= 1280) {
+                if (scroll_percent >= 40 && scroll_percent < 73 || scroll_percent >= 96) {
+                    $("body").css('background-color', '#000000');
+                } else if (scroll_percent >= 73 && scroll_percent < 96) {
+                    $("body").css('background-color', '#ffffff');
+                } else {
+                    $("body").css('background-color', '#ffffff');
+                }
             } else {
-                $("body").css('background-color', '#ffffff');
-            }
+                if (scroll_percent >= 25 && scroll_percent < 65 || scroll_percent >= 91) {
+                    $("body").css('background-color', '#000000');
+                } else if (scroll_percent >= 65 && scroll_percent < 91) {
+                    $("body").css('background-color', '#ffffff');
+                } else {
+                    $("body").css('background-color', '#ffffff');
+                }
+            };
         });
+    });
+
+    //body 배경색에 따른 header 글자 색상 변경
+    $(document).scroll(function () {
+        let bodyBackgroundColor = $("body").css('background-color');
+        let header = $("header");
+        console.log(bodyBackgroundColor)
+
+        if (bodyBackgroundColor != 'rgb(255, 255, 255)') {
+            header.css('color', 'rgb(255, 255, 255)');
+        } else if (bodyBackgroundColor != 'rgb(0, 0, 0)') {
+            header.css('color', 'rgb(0, 0, 0)');
+        } else if ($(window).scrollTop() === 0) {
+            header.css('color', 'rgb(255, 255, 255)');
+        }
     });
 
     //프로젝트 메뉴 클릭 시 아이템 필터
@@ -55,42 +100,49 @@ $(function () {
         });
     });
 
-    $("header nav ul li a").click(function () {
-        $("header nav ul li a").removeClass('on');
-        $(this).addClass('on');
-    });
+    //메뉴 클릭 시 메뉴 스타일 변경
+    // $("header nav ul li a").click(function () {
+    //     $("header nav ul li a").removeClass('on');
+    //     $(this).addClass('on');
+    // });
 
     //마우스 따라다니는 효과
-    $('body').mousemove(function(e){
-        xVal = e.pageX;
-        yVal = e.pageY;
-        gsap.to('.cursor',{
-          x:xVal,
-          y:yVal,
-          duration:.3,
-        })
-      })
+    // $('body').mousemove(function(e){
+    //     xVal = e.pageX;
+    //     yVal = e.pageY;
+    //     gsap.to('.cursor',{
+    //       x:xVal,
+    //       y:yVal,
+    //       duration:.3,
+    //     })
+    //   })
 
-      $('header .nav ul li a').mouseover(function(){
-        gsap.to('.cursor',{backgroundColor:'#0f0', duration:.3})
-        gsap.to('.cursor',{scale:.8})
-      })
-      $('header .nav ul li a').mouseleave(function(){
-        gsap.to('.cursor',{backgroundColor:'#fff',})
-        gsap.to('.cursor',{scale:1})
-      })
-      
-      $('.project_item .image_area').mouseover(function(){
-        gsap.to('.cursor',{scale:2})
-        gsap.to('.cursor span', { visibility: "visible", opacity: 1})
-      })
-      $('.project_item .image_area').mouseleave(function(){
-        gsap.to('.cursor',{scale:1})
-        gsap.to('.cursor span', { visibility: "hidden", opacity: 0,})
-      })
+    //   $('header .nav ul li a').mouseover(function(){
+    //     gsap.to('.cursor',{backgroundColor:'#0f0', duration:.3})
+    //     gsap.to('.cursor',{scale:.8})
+    //   })
+    //   $('header .nav ul li a').mouseleave(function(){
+    //     gsap.to('.cursor',{backgroundColor:'#fff',})
+    //     gsap.to('.cursor',{scale:1})
+    //   })
+
+    //   $('.project_item .image_area').mouseover(function(){
+    //     gsap.to('.cursor',{scale:2})
+    //     gsap.to('.cursor span', { visibility: "visible", opacity: 1})
+    //   })
+    //   $('.project_item .image_area').mouseleave(function(){
+    //     gsap.to('.cursor',{scale:1})
+    //     gsap.to('.cursor span', { visibility: "hidden", opacity: 0,})
+    //   })
 
     //메뉴 클릭 시 해당 섹션으로 부드럽게 이동. 
     $('.header nav ul li a').click(function (e) {
         $('html,body').animate({ scrollTop: $(this.hash).offset().top }, 500);
     })
+    $('.intro_scroll a').click(function (e) {
+        $('html,body').animate({ scrollTop: $(this.hash).offset().top }, 500);
+    });
+
+    
+
 });
